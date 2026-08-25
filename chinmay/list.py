f46ff51dbfb1baed7969c7aaf8b564f414097bbf
Chinmayananda Dhupad <chinmaydhupad@gmail.com>
2026-08-25 23:32:33 +0530

    created the class list(insert func, show func), and now the code in main.py is working git add .!

--- chinmay/list.py
+++ chinmay/list.py
@@ -6,3 +6,22 @@ class Node:
 class list:
     def __init__(self):
         self.head = None
+    def insert(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return 
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+    def show(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(str(current.data))
+            current = current.next
+        if elements:
+            print(" -> ".join(elements))
+        else:
+            print("Empty List")
